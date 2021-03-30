f7934bf1f3118ea262c012681ed8203a3b348147
marcosorensen1 <73217384+marcosorensen1@users.noreply.github.com>
2021-03-30 10:39:29 +0200

    test marco

--- 3_semester_projekt/3_semester_projekt/Program.cs
+++ 3_semester_projekt/3_semester_projekt/Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 //Hello from torben
+//hej fra marco :)
 
 namespace _3_semester_projekt {
     public class Program {

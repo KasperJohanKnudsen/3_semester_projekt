649bad17e50a090e9585e847c459ddd8cc84f189
KasperJohanKnudsen <73699890+KasperJohanKnudsen@users.noreply.github.com>
2021-05-06 13:59:08 +0200

    CreateShowing form start
    start of create showing form, just basic form now

---
+++ 3_semester_projekt/DesktopClient/GuiLayer/CreateShowing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DesktopClient {
+    public partial class CreateShowing : Form {
+        public CreateShowing() {
+            InitializeComponent();
+        }
+    }
+}
